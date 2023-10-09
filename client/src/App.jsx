d1647194a5cdef0ee4da2282e867ea7d6c8a945c
Philipp Mulfinger <info@phil-splash.de>
2023-10-09 16:06:32 +0200

    new commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ScoreBoard from "./components/ScoreBoard";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <img src="src/assets/poke_fight_logo.png" alt="" />
+      <ScoreBoard />
 {/*       <nav>
         <ul>
           <li>
---
+++ client/src/components/ScoreBoard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ScoreBoard() {
+  return (
+    <div>
+      <h1>Hello from the ScoreBoard</h1>
+    </div>
+  );
+}
+
+export default ScoreBoard;
