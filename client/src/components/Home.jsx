16a8d39977edea40919e0f0520e74f8c654477cc
freudinsky <timo.fuchs94@icloud.com>
2023-10-10 16:27:33 +0200

    ..

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,12 @@
 import {Link} from "react-router-dom";
+import "./Select.css"
 function Home() {
 	return (
 		<>
-			<Link to={"/select"}>Start Game</Link>
-			<Link to={"/highscore"}>Leaderboard</Link>
+			<div className="nav">
+				<Link to={"/select"}>Start Game</Link>
+				<Link to={"/highscore"}>Leaderboard</Link>
+			</div>
 		</>
 	);
 }
