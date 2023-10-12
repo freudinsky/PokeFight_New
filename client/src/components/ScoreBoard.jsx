8387244f31b2ac6b2e194f986354807b361c0b61
Philipp Mulfinger <info@phil-splash.de>
2023-10-12 15:02:10 +0200

    Added Pokemons to ScoreBoard

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from "react";
+import { format } from "date-fns";
 import "./ScoreBoard.css";
 import axios from "axios";
 import PokeLogo from "./PokeLogo";
@@ -8,7 +9,7 @@ function ScoreBoard() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8000/game/leaderboard")
+      .get(`${import.meta.env.VITE_API_URL}/game/leaderboard`)
       .then((res) => {
         setGames(res.data);
         console.log(res.data);
@@ -16,27 +17,32 @@ function ScoreBoard() {
       .catch((err) => console.log(err));
   }, []);
 
-  /* useEffect(() => {
-    if (games) {
-      document.body.style.backgroundImage = "url(stadium.webp)"; // double check my quotes
-    }
-  }, [games]); */
-
   return (
     <div className="ScoreBoardWrapper">
       <PokeLogo />
-      <h1>HIGHSCORE</h1>
-      {games.map((item, index) => (
-        <div className="ScoreContainer" key={index}>
-          <h2>
-            Fight: {item.player1} against {item.player2}
-          </h2>
-          <h2>Winner: {item.winner}</h2>
-          <h2>Turns: {item.turns}</h2>
-          <h4>Fight Date: {item.date}</h4>
-          {/* <h2>{`Turns: ${item.turns}, Winner: ${item.winner}, Date: ${item.date}`}</h2> */}
-        </div>
-      ))}
+      <h1>HIGHSCORES</h1>
+      <div className="ScoreContainer_outer">
+        {games.map((item, index) => {
+          const formattedDate = item.date
+            ? format(new Date(item.date), "MMM dd, yyyy")
+            : "";
+          return (
+            <div className="ScoreContainer" key={index}>
+              <div className="ScoreContainer_stats">
+                <h2>
+                  Fight: {item.player1} VS {item.player2}
+                </h2>
+                <h2>Winner: {item.winner}</h2>
+                <h2>Turns: {item.turns}</h2>
+                <h2>Fight Date: {formattedDate}</h2>
+              </div>
+              <div className="ScoreContainer_img">
+                <img src={item.img_Url} alt={item.winner}></img>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
