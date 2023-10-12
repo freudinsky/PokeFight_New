564b0e16ae1a338654019b747a340ed9d179f35f
freudinsky <timo.fuchs94@icloud.com>
2023-10-12 12:33:35 +0200

    ..

--- client/src/components/PokeCard.jsx
+++ client/src/components/PokeCard.jsx
@@ -25,7 +25,7 @@ function PokeCard({ pokemon, sel, setSel }) {
               {
                 <img
                   className="typeIcon"
-                  src={`/typeIcons/${e}.svg`}
+                  src={`../public/typeIcons/${e}.svg`}
                 />
               }{" "}
               {e}
