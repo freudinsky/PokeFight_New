985934fae7504c0165acb974fb699a5c15754e86
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-10-12 07:23:20 +0200

    moved bootstrap-css to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import GameScreen from "./components/GameScreen";
 import Home from "./components/Home";
 import PokeDetail from "./components/PokeDetail";
 import PokeSuperDetail from "./components/PokeSuperDetail";
 import ScoreBoard from "./components/ScoreBoard";
 import Select from "./components/Select";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 /* import ScoreBoard from "./components/ScoreBoard"; */
 
-
-
 function App() {
-	const [userSelection, setUserSelection] = useState();
+  const [userSelection, setUserSelection] = useState();
 
-	return (
-		<>
-			{/* <nav>
+  return (
+    <>
+      {/* <nav>
         <ul>
           <li>
             <Link to={"/"}>Home</Link>
@@ -26,19 +25,27 @@ function App() {
           </li>
         </ul>
       </nav> */}
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/pokemon/:id" element={<PokeDetail />} />
-				<Route path="/pokemon/:id/:info" element={<PokeSuperDetail />} />
-				<Route
-					path="/game"
-					element={<GameScreen pokemonA1={userSelection} />}
-				/>
-				<Route path="/select" element={<Select selection={userSelection} setUserSelection={setUserSelection} />} />
-				<Route path="/highscore" element={<ScoreBoard />} />
-			</Routes>
-		</>
-	);
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:id" element={<PokeDetail />} />
+        <Route path="/pokemon/:id/:info" element={<PokeSuperDetail />} />
+        <Route
+          path="/game"
+          element={<GameScreen pokemonA1={userSelection} />}
+        />
+        <Route
+          path="/select"
+          element={
+            <Select
+              selection={userSelection}
+              setUserSelection={setUserSelection}
+            />
+          }
+        />
+        <Route path="/highscore" element={<ScoreBoard />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
